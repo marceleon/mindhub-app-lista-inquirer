e09667b940f4e66e53dde4e1a692a64deeab447e
marceleon <marcelo.a.leon@gmail.com>
2022-02-06 20:47:32 -0300

    Tarea 10

---
+++ app-10.js
@@ -0,0 +1,9 @@
+// Referncias
+const { Archivo } = require('./helpers/db');
+const { Listado } = require('./models/listado');
+
+const tareas = new Listado();
+const arch = new Archivo(__dirname.concat('/database'), 'ListaDeTareas.json');
+
+tareas.lista = arch.leer();
+console.log(tareas.listadoCompleto());
