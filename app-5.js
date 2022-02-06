389cd00ebd37c1ea83b38229c7cd122b45093d51
marceleon <marcelo.a.leon@gmail.com>
2022-02-06 20:30:47 -0300

    Tarea 5

--- app-5.js
+++ app-5.js
@@ -1,5 +1,18 @@
 // Referncias
 const { Tarea } = require('./models/tarea');
+const { Listado } = require('./models/listado');
 
-const t = new Tarea('Nueva Tarea');
+const t = new Tarea('Tarea UNO');
+
+console.log('Tarea:');
 console.log(t);
+
+const lst = new Listado();
+
+lst.agregarTarea('Tarea DOS');
+lst.agregarTarea('Tarea TRES');
+
+console.log('-----------------');
+console.log('Listado de Tareas');
+console.log(lst);
+console.log(lst.lista);
